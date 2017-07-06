c36e1c9745cf51d48af9a9ed86e61d2cf67a704f
himaniexcellence <himani@excellencetechnologies.in>
2017-07-06 15:36:35 +0530

    index

---
+++ index.php
@@ -0,0 +1,64 @@
+<html>
+<head>
+	<title>Himani Training Assignments</title>
+	<meta charset="UTF-8">
+	<style type="text/css"> 
+		body{
+			text-align: center;
+			background-color: black;
+		}
+		span{
+			font-size: 30px;
+			color: #ffffff;
+			font-weight: bold;
+			margin-top: 5%;
+		}
+		table{
+			margin-top: 5%;
+		}
+		a{
+			text-decoration: none;
+			font-size: 25px;
+			color: #ffffff;
+		}
+		tr td{
+			padding-top: 25%;
+			color: #ffffff;
+			font-size: 25px;
+			width: 100%;
+		}
+		td span{
+			margin-right: 10%;
+		}
+	</style>
+</head>
+<body>
+	<span>Welcome</span>
+	<table align="center">
+		<tr>
+			<td>
+				<span>&#9992;</span><a href="/himani/jQuery/ajax/index.php">Ajax</a>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<span>&#9992;</span><a href="/himani/jQuery/basic/index.php">Basics</a></td>
+		</tr>
+		<tr>
+			<td>
+				<span>&#9992;</span><a href="/himani/jQuery/jquery Plugins/index.php">jQueryPlugins</a>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<span>&#9992;</span><a href="/himani/jQuery/json/index.php">JSON</a>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<span>&#9992;</span><a href="/himani/jQuery/traversing/index.php">Traversing</a>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
